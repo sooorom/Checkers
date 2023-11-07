d31dcafd5beb357c425637233e7a5251ef8c9c98
sooorom <soooromaa@gmail.com>
2023-11-07 15:48:07 +0200

    implemented peace

--- model/Peace.java
+++ model/Peace.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Peace {
+    private boolean isKing;
+    private String color;
 
+    public Peace(String color) {
+        this.color = color;
+    }
+
+    public void setIsKing(boolean isKing) {
+        this.isKing = isKing;
+    }
+
+    public boolean getIsKing() {
+        return isKing;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
