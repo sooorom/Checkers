d69ef6445a177f5eca52fae17f40695d7ae30c88
sooorom <soooromaa@gmail.com>
2023-11-09 19:08:44 +0200

    cell update

--- model/Cell.java
+++ model/Cell.java
@@ -6,13 +6,26 @@ public class Cell {
     final int y;
     final String color;
 
+    public Cell() {
+
+    }
+
     public Cell(Peace peace) {
         this.peace = peace;
     }
 
-    public void removePeace() {
-
+    public void setPeace(Peace peace) {
+        this.peace = peace;
     }
 
-    public void setPeace.
+    public Peace getPeace() {
+        return peace;
+    }
+
+    public void removePeace() {
+        this.peace = null;
+        
+    }
+
+
 }
