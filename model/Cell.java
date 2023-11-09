42888e1ed6c236a4edd305e7f40299e23f734481
sooorom <soooromaa@gmail.com>
2023-11-09 18:36:35 +0200

    cell

--- model/Cell.java
+++ model/Cell.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Cell {
+    private Peace peace;
+    final int x;
+    final int y;
+    final String color;
 
+    public Cell(Peace peace) {
+        this.peace = peace;
+    }
+
+    public void removePeace() {
+
+    }
+
+    public void setPeace.
 }
